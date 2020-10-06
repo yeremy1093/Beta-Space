a3f8cb6beac140d08463007933422a16f93dd97c
yeremy1093 <gah1093@gmail.com>
2020-10-05 17:57:34 -0700

    agregado nuevo background
    agregado background disco

--- Source/Background/background.lua
+++ Source/Background/background.lua
@@ -4,9 +4,10 @@ Background = Class{}
 local blackhole = love.graphics.newImage('Imagen/Background/Black Hole.png')
 local azul = love.graphics.newImage('Imagen/Background/Azul.png')
 local bigeye = love.graphics.newImage('Imagen/Background/Gran OJO.png')
+local disco = love.graphics.newImage('Imagen/Background/Disco.png')
 
 --Numero para saber cual imagen se queda hasta el final
-local Num = love.math.random(1, 3)
+local Num = love.math.random(1, 4)
 
 function Background:init()
 	if Num == 1 then 
@@ -15,6 +16,8 @@ function Background:init()
 		self.loopingStars = azul
 	elseif Num == 3 then
 		self.loopingStars = bigeye
+	elseif Num == 4 then
+		self.loopingStars = disco
 	end
 
 end	
